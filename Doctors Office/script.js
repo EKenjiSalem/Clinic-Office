b288e33db6a4d6a1ba902507106c557401035f53
Elias K. Salem <EliasKSalemTech@gmail.com>
2022-05-28 18:07:50 -0700

    Add files via upload

---
+++ Doctors Office/script.js
@@ -0,0 +1,23 @@
+AOS.init({
+    duration: 1000,
+    delay: 200
+  })
+  
+$(document).ready( () => {
+     $('.fa-bars').click(function() {
+         $(this).toggleClass('fa-times');
+         $('.nav').toggleClass('nav-toggle');
+      });
+
+      $(window).on('load scroll', function() {
+          $('.fa-bars').removeClass('fa-times');
+          $('.nav').removeClass('nav-toggle');
+
+          if($(window).scrollTop() > 10) {
+           $('header').addClass('header-active');
+            } else {
+           $('header').removeClass('header-active');
+          }
+      });
+ });
+
